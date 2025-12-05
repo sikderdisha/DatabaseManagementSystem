4c9f10176a8717fcbd9b815fe6875d71cc435fe9
Disha Sikder <sikderdisha715@gmail.com>
2025-12-05 23:08:32 +0600

    Create student and teacher tables in SQL

---
+++ CreateTable.sql
@@ -0,0 +1,14 @@
+-- create table
+CREATE TABLE student
+(
+id INT PRIMARY KEY,
+name VARCHAR (50),
+age INT NOT NULL
+);
+
+CREATE TABLE teacher
+(
+id INT PRIMARY KEY,
+name VARCHAR (50),
+dept VARCHAR(50)
+);
