ed53ff29dbb1942dc18aedf89b7c976fda57ccee
Disha Sikder <sikderdisha715@gmail.com>
2025-12-05 23:09:37 +0600

    Enhance CreateTable.sql with data insertion and DB management
    Added SQL commands to insert data and manage databases.

--- CreateTable.sql
+++ CreateTable.sql
@@ -12,3 +12,22 @@ id INT PRIMARY KEY,
 name VARCHAR (50),
 dept VARCHAR(50)
 );
+
+-- insert data
+INSERT INTO student VALUES(1,"Disha",20);
+INSERT INTO student VALUES(2,"Munia",25);
+INSERT INTO student VALUES(3,"Milon",22);
+INSERT INTO student VALUES(4,"Maya",32);
+
+-- print table info
+SELECT * FROM student;
+
+-- if not exsit,give warning not error
+CREATE DATABASE IF NOT EXISTS studentTable;
+DROP DATABASE IF EXISTS mytable;
+
+-- show database
+SHOW DATABASES;
+
+-- show tables
+SHOW TABLES;
